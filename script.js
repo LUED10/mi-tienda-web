4661e53a6a11cc7f37b6e8c78018493db859d1bb
LUED10 <luiseduardobravo32@gmail.com>
2026-03-24 00:31:33 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function comprar(producto, precio) {
-    const numero = "51924890953"; // 👉 PON TU NÚMERO REAL
+    const numero = "51924890953"; // Numero de celular
     const mensaje = `Hola, quiero comprar el producto: ${producto} por S/ ${precio}`;
     const url = `https://wa.me/${numero}?text=${encodeURIComponent(mensaje)}`;
 
